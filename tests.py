0edf93ffd627cadc1d6ebad7e371e0b6c0bd8fd2
Emre Sevinç <emre.sevinc@gmail.com>
2010-07-19 19:20:04 +0200

    Added initial unit tests.

---
+++ tests.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import deasciifier
+import unittest
+
+class TestDeasciifierFunctions(unittest.TestCase):
+    
+    def setUp(self):
+        self.ascii_strings = [
+            u"Acimasizca acelya gorunen bir sacmaliktansa acilip sacilmak...",
+            u"Acisindan bagirip cagirarak sacma sozler soylemek.",
+            u"Bogurtuler opucukler.",
+            u"BUYUKCE BIR TOPAC TOPARLAGI VE DE YUMAGI yumagi.",
+            u"Bilgisayarlarda uc adet bellek turu bulunur. Islemci icerisinde yer alan yazmaclar, son derece hizli ancak cok sinirli hafizaya sahiptirler. Islemcinin cok daha yavas olan ana bellege olan erisim gereksinimini gidermek icin kullanilirlar. Ana bellek ise Rastgele erisimli bellek (REB veya RAM, Random Access Memory) ve Salt okunur bellek (SOB veya ROM, Read Only Memory) olmak uzere ikiye ayrilir. RAM'a istenildigi zaman yazilabilir ve icerigi ancak guc surdugu surece korunur. ROM'sa sâdece okunabilen ve onceden yerlestirilmis bilgiler icerir. Bu icerigi gucten bagimsiz olarak korur. Ornegin herhangi bir veri veya komut RAM'da bulunurken, bilgisayar donanimini duzenleyen BIOS ROM'da yer alir.",
+            u"1969 yilinda 15 yasindayken 1000 lira transfer parasi alarak Camialti Spor Kulubu'nde amator futbolcu oldu. Daha sonra IETT Spor Kulubu'nun amator futbolcusu oldu. 1976 yilinda, IETT sampiyon oldugunda, Erdogan da bu takimda oynamaktaydi. Erokspor Kulubunde de futbola devam etti ve 16 yillik futbol yasamini 12 Eylul 1980 Askeri Darbesi sonrasinda birakti ve daha fazla siyasi faaliyet..."]
+
+        self.turkish_strings = [
+            u"Acımasızca açelya görünen bir saçmalıktansa açılıp saçılmak...",
+            u"Acısından bağırıp çağırarak saçma sözler söylemek.",
+            u"Böğürtüler öpücükler.",
+            u"BÜYÜKÇE BİR TOPAÇ TOPARLAĞI VE DE YUMAĞI yumağı.",
+            u"Bilgisayarlarda üç adet bellek turu bulunur. İşlemci içerisinde yer alan yazmaçlar, son derece hızlı ancak çok sınırlı hafızaya sahiptirler. İşlemcinin çok daha yavaş olan ana bellege olan erişim gereksinimini gidermek için kullanılırlar. Ana bellek ise Rastgele erişimli bellek (REB veya RAM, Random Access Memory) ve Salt okunur bellek (SOB veya ROM, Read Only Memory) olmak üzere ikiye ayrılır. RAM'a istenildiği zaman yazılabilir ve içeriği ancak güç sürdüğü sürece korunur. ROM'sa sâdece okunabilen ve önceden yerleştirilmiş bilgiler içerir. Bu içeriği güçten bağımsız olarak korur. Örneğin herhangi bir veri veya komut RAM'da bulunurken, bilgisayar donanımını düzenleyen BİOS ROM'da yer alır.",
+            u"1969 yılında 15 yaşındayken 1000 lira transfer parası alarak Camialtı Spor Kulübü'nde amatör futbolcu oldu. Daha sonra İETT Spor Kulübü'nün amatör futbolcusu oldu. 1976 yılında, İETT şampiyon olduğunda, Erdoğan da bu takımda oynamaktaydı. Erokspor Kulübünde de futbola devam etti ve 16 yıllık futbol yaşamını 12 Eylül 1980 Askeri Darbesi sonrasında bıraktı ve daha fazla siyasi faaliyet..."]
+
+    def test_convert_to_turkish(self):
+        for i in range(len(self.ascii_strings)):
+            dea = deasciifier.Deasciifier(self.ascii_strings[i]) 
+            result = dea.convert_to_turkish()
+            self.assertEqual(result, self.turkish_strings[i])
+
+if __name__ == '__main__':
+    unittest.main()
