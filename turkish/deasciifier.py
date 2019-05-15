35af840ba0c600a29aa63c7376d9ffaf5023839f
Orkin31 <50326588+Orkin31@users.noreply.github.com>
2019-05-15 11:49:50 +0300

    updated uppercase to python3

--- turkish/deasciifier.py
+++ turkish/deasciifier.py
@@ -3323,7 +3323,7 @@ class Deasciifier:
                                u'Ş': u'S'}
 
       turkish_downcase_asciify_table = {}
-      for ch in string.uppercase:
+      for ch in string.ascii_uppercase:
             turkish_downcase_asciify_table[ch] = ch.lower()
             turkish_downcase_asciify_table[ch.lower()] = ch.lower()
       turkish_downcase_asciify_table[u'ç'] = u'c'
@@ -3340,7 +3340,7 @@ class Deasciifier:
       turkish_downcase_asciify_table[u'Ü'] = u'u'
 
       turkish_upcase_accents_table = {}
-      for ch in string.uppercase:
+      for ch in string.ascii_uppercase:
             turkish_upcase_accents_table[ch] = ch.lower()
             turkish_upcase_accents_table[ch.lower()] = ch.lower()
       turkish_upcase_accents_table[u'ç'] = u'C'
